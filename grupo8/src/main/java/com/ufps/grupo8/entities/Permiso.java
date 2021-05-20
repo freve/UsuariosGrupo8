cd4cc554b506313025d80e64415b155b1c51cea5
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2021-05-19 21:45:35 -0500

    Base del proyecto con conexion a la bd

---
+++ grupo8/src/main/java/com/ufps/grupo8/entities/Permiso.java
@@ -0,0 +1,76 @@
+package com.ufps.grupo8.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="permiso")
+public class Permiso implements Serializable {
+
+	
+	@Id
+	@GeneratedValue
+	Integer id;
+	
+	@Column
+	String nombre;
+	
+	@Column
+	Date fechaModificacion;
+	
+	@Column
+	boolean autorizacion;
+	
+	@OneToMany
+	List<Funcionalidad> funcionalidades;
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaModificacion() {
+		return fechaModificacion;
+	}
+
+	public void setFechaModificacion(Date fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+
+	public boolean isAutorizacion() {
+		return autorizacion;
+	}
+
+	public void setAutorizacion(boolean autorizacion) {
+		this.autorizacion = autorizacion;
+	}
+
+	public List<Funcionalidad> getFuncionalidades() {
+		return funcionalidades;
+	}
+
+	public void setFuncionalidades(List<Funcionalidad> funcionalidades) {
+		this.funcionalidades = funcionalidades;
+	}
+   
+}
