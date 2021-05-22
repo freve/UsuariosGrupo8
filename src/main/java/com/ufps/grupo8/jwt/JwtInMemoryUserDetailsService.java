1c6f1a5afa7e7c693137c396aeaede3fe7bf58ef
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2021-05-21 21:01:32 -0500

    "JWT montados"

---
+++ src/main/java/com/ufps/grupo8/jwt/JwtInMemoryUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.ufps.grupo8.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtInMemoryUserDetailsService implements UserDetailsService {
+
+  static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
+
+  static {
+    inMemoryUserList.add(new JwtUserDetails(1L, "Ivan",
+        "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+    inMemoryUserList.add(new JwtUserDetails(2L, "Grupo8",
+            "$2a$10$.0ACb6Kd1126YoGaw6CSJOWd5zjHI5n9XalFuTyneLt1BvD2S2YVS", "ROLE_USER_2"));
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<JwtUserDetails> findFirst = inMemoryUserList.stream()
+        .filter(user -> user.getUsername().equals(username)).findFirst();
+
+    if (!findFirst.isPresent()) {
+      throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
+    }
+
+    return findFirst.get();
+  }
+
+}
+
+
